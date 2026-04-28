8d907fbfe50441b15c40bce91682dd64017ba809
Gabriel Lourencetti <gabriel.pllegleg@gmail.com>
2026-04-28 00:11:54 -0300

    add: configuração inicial do banco de dados

--- todo-api/db.js
+++ todo-api/db.js
@@ -1,1 +1,18 @@
 // A função desse arquivo é conectar o mysql com o codigo
+const mysql = require('mysql2'); // importa o mysql2 para conectar ao banco de dados
+
+// configuração do banco de dados
+const db = mysql.createConnection({ 
+    host: 'localhost',
+    user: 'root',  // usuario do banco
+    password: '', // senha do banco 
+    database: 'todo_db'
+});
+
+// Conectando ao banco de dados
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Conectado ao banco de dados MySQL!');
+});
+
+module.exports = db; // exporta a conexão para ser usada em outros arquivos
